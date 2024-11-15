921790fe505b1c2085768eae709433c47d11ad71
Q <99633210+Quathar@users.noreply.github.com>
2024-11-15 16:50:17 +0100

    Unify build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,3 +40,34 @@ subprojects {
     }
 
 }
+
+project(":contactbook-data") {
+    dependencies {
+        // [ H2 Driver ]
+        implementation("com.h2database:h2:2.1.214")
+        testImplementation("com.h2database:h2:2.1.214")
+
+        // Google Gson :: JSON to Java and vice versa
+        // https://mvnrepository.com/artifact/com.google.code.gson/gson
+        implementation("com.google.code.gson:gson:2.10.1")
+
+        // Hibernate ORM
+//        implementation("org.hibernate:hibernate-core:5.6.15.Final")
+//        implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+    }
+}
+
+project(":contactbook-application") {
+    group = "com.quathar"
+    version = "1.0-SNAPSHOT"
+
+    dependencies {
+        implementation(project(":contactbook-data"))
+
+        // JetBrains Themes
+        implementation("com.formdev:flatlaf:3.0")
+
+        // JCalendar component
+        implementation("com.toedter:jcalendar:1.4")
+    }
+}
