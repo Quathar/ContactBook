581e09c9ecbc8f01ae9c6727e144a69aeed14c27
Q <99633210+Quathar@users.noreply.github.com>
2024-11-15 17:58:27 +0100

    refactor() Update ResourceNotFoundException

--- contactbook-data/src/main/java/com/quathar/contactbook/data/exception/ResourceNotFoundException.java
+++ contactbook-data/src/main/java/com/quathar/contactbook/data/exception/ResourceNotFoundException.java
@@ -3,27 +3,15 @@ package com.quathar.contactbook.data.exception;
 /**
  * <h1>Resource NOT FOUND Exception</h1>
  *
+ * @see RuntimeException
  * @since 2023-03-30
  * @version 1.0
  * @author Q
  */
 public class ResourceNotFoundException extends RuntimeException {
 
-    // <<-CONSTRUCTORS->>
     public ResourceNotFoundException() {
         super();
-    }
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-
-    public ResourceNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public ResourceNotFoundException(Throwable cause) {
-        super(cause);
     }
 
 }
