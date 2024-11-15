a0d08d8d928f1da2a316d1a21695176b8e35b959
Q <99633210+Quathar@users.noreply.github.com>
2024-11-15 17:29:58 +0100

    update() Update Contact & Hobby DAO implementations

--- contactbook-data/src/main/java/com/quathar/contactbook/data/dao/impl/ContactDaoImpl.java
+++ contactbook-data/src/main/java/com/quathar/contactbook/data/dao/impl/ContactDaoImpl.java
@@ -1,54 +1,31 @@
 package com.quathar.contactbook.data.dao.impl;
 
 import com.google.inject.Inject;
+
 import com.quathar.contactbook.data.dao.ContactDao;
 import com.quathar.contactbook.data.entity.Contact;
 import com.quathar.contactbook.data.enumerator.ContactType;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
-import jakarta.persistence.RollbackException;
-
 import java.util.List;
-import java.util.Optional;
 
 /**
  * <h1>Contact DAO Implementation</h1>
  *
- * @since 2023-05-30
  * @see ContactDao
+ * @see GeneralDaoImpl
+ * @since 2023-05-30
  * @version 1.0
  * @author Q
  */
-public class ContactDaoImpl implements ContactDao {
+public class ContactDaoImpl extends GeneralDaoImpl<Contact, Long> implements ContactDao {
 
-    // <<-FIELDS->>
-    private final SessionFactory _sessionFactory;
-
-    // <<-CONSTRUCTOR->>
     @Inject
     public ContactDaoImpl(Configuration configuration) {
-        _sessionFactory = configuration.buildSessionFactory();
-    }
-
-    // <<-METHODS->>
-    @Override
-    public List<Contact> findAll() {
-        try (Session session = _sessionFactory.openSession()) {
-            Query<Contact> query = session.createQuery("FROM Contact", Contact.class);
-            return query.list();
-        }
-    }
-
-    @Override
-    public Optional<Contact> findById(Long id) {
-        try (Session session = _sessionFactory.openSession()) {
-            return Optional.of(session.get(Contact.class, id));
-        }
+        super(configuration.buildSessionFactory(), Contact.class);
     }
 
     @Override
@@ -64,57 +41,6 @@ public class ContactDaoImpl implements ContactDao {
             query.setParameter("name", name);
 
             return query.list();
-        }
-    }
-
-    @Override
-    public Contact save(Contact contact) {
-        Transaction transaction = null;
-        try (Session session = _sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            contact = session.merge(contact);
-            transaction.commit();
-            return contact;
-        } catch (IllegalStateException | RollbackException ex) {
-            if (transaction != null)
-                transaction.rollback();
-            return null;
-        }
-    }
-
-    @Override
-    public void deleteAll() {
-        Transaction transaction = null;
-        try (Session session = _sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            Query<Contact> query = session.createQuery("DELETE FROM Contact", Contact.class);
-            query.executeUpdate();
-            transaction.commit();
-        } catch (IllegalStateException | RollbackException ex) {
-            if (transaction != null)
-                transaction.rollback();
-        }
-    }
-
-    @Override
-    public void deleteById(Long id) {
-        Transaction transaction = null;
-        try (Session session = _sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            Contact contact = session.get(Contact.class, id);
-            if (contact != null)
-                session.remove(contact);
-            transaction.commit();
-        } catch (IllegalStateException | RollbackException ex) {
-            if (transaction != null)
-                transaction.rollback();
-        }
-    }
-
-    @Override
-    public boolean existsById(Long id) {
-        try (Session session = _sessionFactory.openSession()) {
-            return session.get(Contact.class, id) != null;
         }
     }
 
--- contactbook-data/src/main/java/com/quathar/contactbook/data/dao/impl/HobbyDaoImpl.java
+++ contactbook-data/src/main/java/com/quathar/contactbook/data/dao/impl/HobbyDaoImpl.java
@@ -1,53 +1,30 @@
 package com.quathar.contactbook.data.dao.impl;
 
 import com.google.inject.Inject;
+
 import com.quathar.contactbook.data.dao.HobbyDao;
 import com.quathar.contactbook.data.entity.Hobby;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
-import jakarta.persistence.RollbackException;
-
 import java.util.List;
-import java.util.Optional;
 
 /**
  * <h1>Hobby DAO Implementation</h1>
  *
- * @since 2023-05-30
  * @see HobbyDao
+ * @see GeneralDaoImpl
+ * @since 2023-05-30
  * @version 1.0
  * @author Q
  */
-public class HobbyDaoImpl implements HobbyDao {
+public class HobbyDaoImpl extends GeneralDaoImpl<Hobby, Long> implements HobbyDao {
 
-    // <<-FIELDS->>
-    private final SessionFactory _sessionFactory;
-
-    // <<-CONSTRUCTOR->>
     @Inject
     public HobbyDaoImpl(Configuration configuration) {
-        _sessionFactory = configuration.buildSessionFactory();
-    }
-
-    // <<-METHODS->>
-    @Override
-    public List<Hobby> findAll() {
-        try (Session session = _sessionFactory.openSession()) {
-            Query<Hobby> query = session.createQuery("FROM Hobby", Hobby.class);
-            return query.list();
-        }
-    }
-
-    @Override
-    public Optional<Hobby> findById(Long id) {
-        try (Session session = _sessionFactory.openSession()) {
-            return Optional.of(session.get(Hobby.class, id));
-        }
+        super(configuration.buildSessionFactory(), Hobby.class);
     }
 
     @Override
@@ -58,57 +35,6 @@ public class HobbyDaoImpl implements HobbyDao {
             Query<Hobby> query = session.createQuery(hql, Hobby.class);
             query.setParameter("name", name);
             return query.list();
-        }
-    }
-
-    @Override
-    public Hobby save(Hobby hobby) {
-        Transaction transaction = null;
-        try (Session session = _sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            hobby = session.merge(hobby);
-            transaction.commit();
-            return hobby;
-        } catch (IllegalStateException | RollbackException ex) {
-            if (transaction != null)
-                transaction.rollback();
-            return null;
-        }
-    }
-
-    @Override
-    public void deleteAll() {
-        Transaction transaction = null;
-        try (Session session = _sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            Query<Hobby> query = session.createQuery("DELETE FROM Hobby", Hobby.class);
-            query.executeUpdate();
-            transaction.commit();
-        } catch (IllegalStateException | RollbackException ex) {
-            if (transaction != null)
-                transaction.rollback();
-        }
-    }
-
-    @Override
-    public void deleteById(Long id) {
-        Transaction transaction = null;
-        try (Session session = _sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            Hobby hobby = session.get(Hobby.class, id);
-            if (hobby != null)
-                session.remove(hobby);
-            transaction.commit();
-        } catch (IllegalStateException | RollbackException ex) {
-            if (transaction != null)
-                transaction.rollback();
-        }
-    }
-
-    @Override
-    public boolean existsById(Long id) {
-        try (Session session = _sessionFactory.openSession()) {
-            return session.get(Hobby.class, id) != null;
         }
     }
 
