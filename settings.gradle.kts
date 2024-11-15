f3df66c5306dcf2ec6c35fa2d3dcf29314aeb45f
Q <99633210+Quathar@users.noreply.github.com>
2024-11-15 16:34:06 +0100

    Clean settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,4 @@
-// Project name
 rootProject.name = "ContactBook"
 
-// Modules
-include(":application")
-include(":data")
-
-// Repositories for plugin resolution
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
-// Repositories for dependencies resolution
-//dependencyResolutionManagement {
-//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-//    repositories {
-//        google()
-//        mavenCentral()
-//    }
-//}
+include(":contactbook-application")
+include(":contactbook-data")
